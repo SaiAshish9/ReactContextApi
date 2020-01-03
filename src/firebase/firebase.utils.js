e8c34c723bcbedf6f99e4a20cb7ebb6da0a3a1e2
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2020-01-03 18:15:12 +0530

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,21 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  // apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  // authDomain: 'crwn-db.firebaseapp.com',
-  // databaseURL: 'https://crwn-db.firebaseio.com',
-  // projectId: 'crwn-db',
-  // storageBucket: 'crwn-db.appspot.com',
-  // messagingSenderId: '850995411664',
-  // appId: '1:850995411664:web:7ddc01d597846f65'
-  apiKey: "AIzaSyCGcIVOZoE2UZIIzGIis1hwM_CZLbJRVKc",
-  authDomain: "graphqldb.firebaseapp.com",
-  databaseURL: "https://graphqldb.firebaseio.com",
-  projectId: "graphqldb",
-  storageBucket: "graphqldb.appspot.com",
-  messagingSenderId: "226883476148",
-  appId: "1:226883476148:web:11e1118a809c91741695e1",
-  measurementId: "G-W83BNE3VKB"
+ 
 };
 
 firebase.initializeApp(config);
